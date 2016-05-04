d5edcf4290f667a0b2658caf41272db87fa9f32e
unknown <lhajdu@HUB-LHAJDU-W7.mgc.mentorg.com>
2016-05-04 10:03:07 +0200

    embedded web chat

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var restify = require('restify');
+var express = require('express');
 var builder = require('botbuilder');
 var index = require('./dialogs/index');
 var build = require('./dialogs/build');
@@ -35,8 +36,17 @@ bot.add('/firstRun', [
 ]);
 
 // Setup Restify Server
-var server = restify.createServer();
-server.post('/api/messages', bot.verifyBotFramework({ appId: 'jenkinsbot', appSecret: '5d11dbef64694e19a7ed919f6ba745ab' }), bot.listen());
-server.listen(process.env.PORT || 8080, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+//var server = restify.createServer();
+var app = express();
+app.locals.title = 'JenkinsBot';
+app.use(bot.verifyBotFramework({ appId: 'jenkinsbot', appSecret: '5d11dbef64694e19a7ed919f6ba745ab' }));
+app.post('/api/messages', bot.listen());
+
+app.set('view engine', 'pug');
+app.get('/', function (req, res) {
+   res.render('index');
+});
+
+var listener = app.listen(process.env.PORT || 8080, function () {
+   console.log('%s listening on %s port', app.locals.title, listener.address().port); 
 });
